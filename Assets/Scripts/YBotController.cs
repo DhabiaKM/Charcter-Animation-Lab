409f71c15b1703540ef96cb9d89a6adadecee0b4
DhabiaKM <44834193+DhabiaKM@users.noreply.github.com>
2021-11-13 12:03:38 +0400

    Done - Blend Trees Not Working

--- Assets/Scripts/YBotController.cs
+++ Assets/Scripts/YBotController.cs
@@ -9,20 +9,30 @@ public class YBotController : MonoBehaviour
     NavMeshAgent agent; 
 
     int isWalkingHash; 
-    
+    int velocityXHash; 
+    int velocityZHash;
     private void Awake( ){ 
         animator = GetComponent<Animator>(); 
         agent = GetComponent<NavMeshAgent>(); 
 
         isWalkingHash = Animator.StringToHash("isWalking"); 
+        velocityXHash = Animator.StringToHash("velocityX"); 
+        velocityZHash = Animator.StringToHash("velocityZ"); 
 
     }
 
     private void Update()
     { 
+        Vector3 velocity = agent.velocity;
+
         bool isMoving = agent.velocity.magnitude> 0.01f && agent.remainingDistance >agent.radius; 
 
-        animator.SetBool("isWalking", isMoving);
+        animator.SetBool(isWalkingHash, isMoving);
+
+        velocity = transform.InverseTransformVector(velocity);
+
+        animator.SetFloat(velocityXHash, velocity.x); 
+        animator.SetFloat(velocityZHash, velocity.z); 
 
     }
     
